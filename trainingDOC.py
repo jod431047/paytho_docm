f00752eaee0335da223b06dcf7603291e2f7520f
jod431047 <jod431047@gmail.com>
2023-10-29 19:57:07 +0100

    false

---
+++ trainingDOC.py
@@ -0,0 +1,9 @@
+# Verwendung des Schlüsselworts "False"
+bedingung = False
+
+if bedingung:
+    print("Die Bedingung ist wahr!")
+else:
+    print("Die Bedingung ist falsch!")
+
+# Ausgabe: Die Bedingung ist falsch!

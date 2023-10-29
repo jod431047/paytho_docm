908da650bcdcdd3b55b5718bc86f1036a518f414
jod431047 <jod431047@gmail.com>
2023-10-30 00:25:27 +0100

    lambda

--- trainingDOC.py
+++ trainingDOC.py
@@ -1,7 +1,9 @@
-def check_number(num):
-    if num > 0:
-        print("Die Zahl ist positiv.")
-    elif num < 0:
-        print("Die Zahl ist negativ.")
-    else:
-        print("Die Zahl ist null.")
+# Beispiel: Addieren von zwei Zahlen mit einer Lambda-Funktion
+addieren = lambda x, y: x + y
+ergebnis = addieren(3, 5)
+print(ergebnis)  # Output: 8
+
+# Beispiel: Quadrieren einer Zahl mit einer Lambda-Funktion
+quadrieren = lambda x: x ** 2
+ergebnis = quadrieren(4)
+print(ergebnis)  # Output: 16

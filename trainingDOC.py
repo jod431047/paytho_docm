ad19c182596aae38bc2b55e54f02a3e3f4c8a03a
jod431047 <jod431047@gmail.com>
2023-10-30 00:09:34 +0100

    or

--- trainingDOC.py
+++ trainingDOC.py
@@ -1,6 +1,7 @@
-x = [1, 2, 3]
-y = [1, 2, 3]
-z = x
+x = 5
+y = 10
 
-print(x is y)  # False, da x und y auf unterschiedliche Objekte verweisen
-print(x is z)  # True, da x und z auf dasselbe Objekt verweisen
+if x > 3 or y < 20:
+    print("Mindestens eine Bedingung ist wahr, wird ausgeführt!")
+else:
+    print("Keine der Bedingungen ist wahr, wird übersprungen!")

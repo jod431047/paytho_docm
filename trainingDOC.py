964de7416f1c590757c71e4af91c9813c89bb98e
jod431047 <jod431047@gmail.com>
2023-10-30 00:48:00 +0100

    with

--- trainingDOC.py
+++ trainingDOC.py
@@ -1,14 +1,3 @@
-def calculate_factorial(n):
-    if n < 0:
-        raise ValueError("Die Fakultät ist nur für nicht-negative Zahlen definiert.")
-    factorial = 1
-    for i in range(1, n + 1):
-        factorial *= i
-    return factorial
-
-try:
-    number = int(input("Gib eine Zahl ein: "))
-    result = calculate_factorial(number)
-    print("Die Fakultät von", number, "ist", result)
-except ValueError as e:
-    print("Fehler:", e)
+with open("datei.txt", "r") as f:
+    content = f.read()
+    print(content)
